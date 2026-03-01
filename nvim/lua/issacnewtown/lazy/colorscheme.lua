beb817c8e8eace2f9f81d46afa84ab2fda24b357
aniketmondal092009 <aniketmondal092009@gmail.com>
2026-03-01 23:46:08 +0530

    fix: colorscheme.lua

--- nvim/lua/issacnewtown/lazy/colorscheme.lua
+++ nvim/lua/issacnewtown/lazy/colorscheme.lua
@@ -2,18 +2,36 @@ return {
     "rebelot/kanagawa.nvim",
     config = function ()
         require('kanagawa').setup({
-            colors = {
+            compile = false,             -- enable compiling the colorscheme
+            undercurl = true,            -- enable undercurls
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = true},
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = false,         -- do not set background color
+            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+            colors = {                   -- add/modify theme and palette colors
+                palette = {},
                 theme = {
                     all = {
                         ui = {
                             bg_gutter = "none"
                         }
                     }
-                }
+                },
             },
-            theme = "dragon",
+
+            overrides = function(colors) -- add/modify highlights
+                return {}
+            end,
+
+            background = {               -- map the value of 'background' option to a theme
+                dark = "dragon",           -- try "dragon" !
+            },
         })
 
-        vim.cmd("colorscheme kanagawa-dragon")
+        vim.cmd("colorscheme kanagawa")
     end
 }
